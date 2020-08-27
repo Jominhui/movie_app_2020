50ac472c9ba5ffb705bcb9942949de9b0db3b2c4
Jominhui <rihito2021@naver.com>
2020-08-27 10:22:27 +0900

    Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 
-const Rapper = ({fav}) =>{
-return <h1>I like {fav}</h1>;
+const Rapper = ({name , picture}) =>{
+  return ( 
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const Rapper_like = [
+  {
+    name: "Ash Island",
+    image: "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDA1MjNfMTk4%2FMDAxNTkwMTYxOTU4NDc5.e0QRts0ZF2A0yFuHGbKu6AYErm7yYRNeLCrLMikGq6Yg.nVr7ZQRCxw6oJcfXwzqX9G5gWyHnNjhi1jy8b-8R1cQg.JPEG.whitue8%2F2.jpg&type=sc960_832"
+  },
+  {
+    name: "Hash Swan",
+    image: "https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F5672%2F2019%2F12%2F22%2F0000026998_001_20191222091415772.jpg&type=sc960_832"
+  },
+  {
+    name: "ChangMo",
+    image: "https://search.pstatic.net/common/?src=http%3A%2F%2Fpost.phinf.naver.net%2FMjAxODA2MjBfOTIg%2FMDAxNTI5NDc5OTAzMzAz.32byngeOTUceAUn1nU2hTEGm-LnpgDg2Ghf9Vv9V3aYg.IfbMY-RVKWhDYvikfIlg5objrbu_mrJ7r2OqhSbNPkwg.JPEG%2FIvaYHws37sfKbGlwW3jzm_ckcrNA.jpg&type=sc960_832"
+  }
+];
 
 function App() {
   return( 
     <div>
-      <h1>Hello!</h1>
-      <Rapper fav="Ash Island" />
-      <Rapper fav="Hash Swan" />
-      <Rapper fav="ChangMo" />
-      <Rapper fav="Haon" />
+      {Rapper_like.map(Rap => <Rapper name={Rap.name} picture={Rap.image}/>)}
     </div>
     );
 }
